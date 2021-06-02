ecaf6f0bf8c626af3631cb39c6ea113964346fc5
Ying <cy240705@gmail.com>
2021-06-01 18:24:49 -0700

    fix reducer

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 const App = () => (
     <div>
-    <h1>Welcome to Ama2on!</h1>
+    <h1>Welcome to Ama2on</h1>
     </div>
 );
 
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
 
+
+const dummyReducer = (state) => {
+    return {}
+}
+
 const RootReducer = combineReducers({
-  
+  dummyReducer
 });
 
 export default RootReducer;
