1a0b7f040bd20f001b53fc40fc7564d49712b8a1
Ying <cy240705@gmail.com>
2021-08-16 02:32:02 -0700

    fixed bug

--- db/migrate/20210815060054_editproduct.rb
+++
@@ -1,5 +0,0 @@
-class Editproduct < ActiveRecord::Migration[5.2]
-  def change
-    change_column :products,:category_id, :integer,null: false
-  end
-end
--- db/migrate/20210816085036_editingid.rb
+++
@@ -1,5 +0,0 @@
-class Editingid < ActiveRecord::Migration[5.2]
-  def change
-    change_column :products,:category_id, :integer, null:true
-  end
-end
